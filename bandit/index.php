81ba1bdd3dc255494d4e382a181eddfdc01ad4af
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2022-11-29 02:33:55 +0700

    add description and generator

--- bandit/index.php
+++ bandit/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="<?php echo Theme::lang() ?>">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<?php echo Theme::metaTagTitle(); ?>
-	<meta name="generator" content="Bludit">
-	<?php echo Theme::metaTagDescription(); ?>
-	<?php echo Theme::favicon('img/favicon.png'); ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php echo Theme::metaTagTitle(); ?>
+<?php echo Theme::metaTagDescription(); ?>
+<?php echo Theme::favicon('img/favicon.png'); ?>
 <?php echo Theme::css('css/bs.css'); ?>
 <?php echo Theme::css('css/bandit.css'); ?>
-	<?php Theme::plugins('siteHead'); ?>
+<?php Theme::plugins('siteHead'); ?>
+<meta name="generator" content="Bludit">
 </head>
 <body>
 	<?php Theme::plugins('siteBodyBegin'); ?>
