c2bbc326201a06220cbde8bce32ade235da55766
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2023-03-09 05:06:34 +0700

    update viewport

--- bandit/index.php
+++ bandit/index.php
@@ -2,7 +2,7 @@
 <html lang="<?php echo Theme::lang() ?>">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php echo Theme::metaTagTitle(); ?>
 <?php echo Theme::metaTagDescription(); ?>
 <?php echo Theme::favicon('img/favicon.png'); ?>
